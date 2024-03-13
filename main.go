dd9fc2f12e7d47ecaa9388b5df9914706f0b0e92
toshiaki <conamn1185@gmail.com>
2024-03-13 14:46:04 +0900

    refactored

--- main.go
+++ main.go
@@ -15,91 +15,123 @@ import (
 )
 
 func main() {
-	loadEnv()
-	channelSecret := os.Getenv("LINE_CHANNEL_SECRET")
-	bot, err := messaging_api.NewMessagingApiAPI(
-		os.Getenv("LINE_CHANNEL_TOKEN"),
-	)
+	if err := loadEnv(); err != nil {
+		log.Fatalf("Error loading environment variables: %v", err)
+	}
+
+	bot, err := setupBot()
 	if err != nil {
-		log.Fatal(err)
-	}
-	chickenEvent := event.NewChickenEvent(bot)
-	defaultEvent := event.NewDefaultEvent(bot)
-	textEvents := []event.TextMessageEvent{
-		chickenEvent, defaultEvent,
+		log.Fatalf("Error setting up bot: %v", err)
 	}
 
-	// Setup HTTP Server for receiving requests from LINE platform
+	textEvents := setupTextEvents(bot)
+
 	http.HandleFunc("/callback", func(w http.ResponseWriter, req *http.Request) {
-		log.Println("/callback called...")
-
-		cb, err := webhook.ParseRequest(channelSecret, req)
-		if err != nil {
-			log.Printf("Cannot parse request: %+v\n", err)
-			if errors.Is(err, webhook.ErrInvalidSignature) {
-				w.WriteHeader(400)
-			} else {
-				w.WriteHeader(500)
-			}
-			return
-		}
-
-		log.Println("Handling events...")
-		for _, callbackRequest := range cb.Events {
-			log.Printf("/callback called%+v...\n", callbackRequest)
-			switch e := callbackRequest.(type) {
-			case webhook.MessageEvent:
-				switch message := e.Message.(type) {
-				case webhook.TextMessageContent:
-					for _, textEvent := range textEvents {
-						if textEvent.IsApplicable(message.Text) {
-							_ = textEvent.Handle(message, e) // error handlinしないと！！
-							break
-						}
-					}
-				case webhook.StickerMessageContent:
-					replyMessage := fmt.Sprintf(
-						"sticker id is %s, stickerResourceType is %s", message.StickerId, message.StickerResourceType)
-					if _, err = bot.ReplyMessage(
-						&messaging_api.ReplyMessageRequest{
-							ReplyToken: e.ReplyToken,
-							Messages: []messaging_api.MessageInterface{
-								messaging_api.TextMessage{
-									Text: replyMessage,
-								},
-							},
-						}); err != nil {
-						log.Print(err)
-					} else {
-						log.Println("Sent sticker reply.")
-					}
-				default:
-					log.Printf("Unsupported message content: %T\n", e.Message)
-				}
-			default:
-				log.Printf("Unsupported message: %T\n", callbackRequest)
-			}
-		}
+		handleCallback(w, req, bot, textEvents)
 	})
 
-	// This is just sample code.
-	// For actual use, you must support HTTPS by using `ListenAndServeTLS`, a reverse proxy or something else.
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "5001"
-	}
-	fmt.Println("http://localhost:" + port + "/")
-	if err := http.ListenAndServe(":"+port, nil); err != nil {
-		log.Fatal(err)
-	}
+	startServer()
 }
 
-func loadEnv() {
+func loadEnv() error {
 	env := os.Getenv("ENV")
 	if env == "" {
 		fileName := ".env.local"
 		if err := godotenv.Load(fileName); err != nil {
-			panic("Error loading " + fileName + " file")
+			return fmt.Errorf("error loading %s file: %w", fileName, err)
 		}
+	}
+	return nil
+}
+
+func setupBot() (*messaging_api.MessagingApiAPI, error) {
+	channelToken := os.Getenv("LINE_CHANNEL_TOKEN")
+	bot, err := messaging_api.NewMessagingApiAPI(channelToken)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create bot: %w", err)
+	}
+	return bot, nil
+}
+
+func setupTextEvents(bot *messaging_api.MessagingApiAPI) []event.TextMessageEvent {
+	chickenEvent := event.NewChickenEvent(bot)
+	defaultEvent := event.NewDefaultEvent(bot)
+	return []event.TextMessageEvent{chickenEvent, defaultEvent}
+}
+
+func handleCallback(w http.ResponseWriter, req *http.Request, bot *messaging_api.MessagingApiAPI, textEvents []event.TextMessageEvent) {
+	log.Println("/callback called...")
+
+	channelSecret := os.Getenv("LINE_CHANNEL_SECRET")
+	cb, err := webhook.ParseRequest(channelSecret, req)
+	if err != nil {
+		log.Printf("Cannot parse request: %+v\n", err)
+		statusCode := http.StatusInternalServerError
+		if errors.Is(err, webhook.ErrInvalidSignature) {
+			statusCode = http.StatusBadRequest
+		}
+		w.WriteHeader(statusCode)
+		return
+	}
+
+	processEvents(cb.Events, bot, textEvents)
+}
+
+func processEvents(events []webhook.EventInterface, bot *messaging_api.MessagingApiAPI, textEvents []event.TextMessageEvent) {
+	log.Println("Handling events...")
+	for _, callbackRequest := range events {
+		handleEvent(callbackRequest, bot, textEvents)
+	}
+}
+
+func handleEvent(callbackRequest webhook.EventInterface, bot *messaging_api.MessagingApiAPI, textEvents []event.TextMessageEvent) {
+	switch e := callbackRequest.(type) {
+	case webhook.MessageEvent:
+		handleMessageEvent(e, bot, textEvents)
+	default:
+		log.Printf("Unsupported message: %T\n", callbackRequest)
+	}
+}
+
+func handleMessageEvent(e webhook.MessageEvent, bot *messaging_api.MessagingApiAPI, textEvents []event.TextMessageEvent) {
+	switch message := e.Message.(type) {
+	case webhook.TextMessageContent:
+		for _, textEvent := range textEvents {
+			if textEvent.IsApplicable(message.Text) {
+				textEvent.Handle(message, e) // Improved error handling is suggested
+				break
+			}
+		}
+	case webhook.StickerMessageContent:
+		sendStickerReply(message, e.ReplyToken, bot)
+	default:
+		log.Printf("Unsupported message content: %T\n", e.Message)
+	}
+}
+
+func sendStickerReply(message webhook.StickerMessageContent, replyToken string, bot *messaging_api.MessagingApiAPI) {
+	replyMessage := fmt.Sprintf("sticker id is %s, stickerResourceType is %s", message.StickerId, message.StickerResourceType)
+	if _, err := bot.ReplyMessage(&messaging_api.ReplyMessageRequest{
+		ReplyToken: replyToken,
+		Messages: []messaging_api.MessageInterface{
+			messaging_api.TextMessage{
+				Text: replyMessage,
+			},
+		},
+	}); err != nil {
+		log.Print(err)
+	} else {
+		log.Println("Sent sticker reply.")
+	}
+}
+
+func startServer() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "5001"
+	}
+	log.Printf("Server starting on http://localhost:%s/\n", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
 	}
 }
